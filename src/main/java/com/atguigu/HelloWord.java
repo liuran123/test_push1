8d0ea05a081e2ab096a2d3b2332bc6b32c8172c8
liuran <liuran@qq.com>
2023-01-07 22:48:30 +0800

    commit 10

--- src/main/java/com/atguigu/HelloWord.java
+++ src/main/java/com/atguigu/HelloWord.java
@@ -8,5 +8,6 @@ public class HelloWord {
         System.out.println("444444——hotfix");
         System.out.println("44444444444444");
         System.out.println("444444444444445555");
+        System.out.println("4444444444444455556666");
     }
 }
